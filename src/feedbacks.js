42e5f12baeb9cf1b6b74d21e94c7e751cce99471
Jeff Walden <jwalden@mit.edu>
2024-05-12 23:45:26 -0700

    refactor: Give muteFdb a better name and inline the single foreground/background color set.

--- src/feedbacks.js
+++ src/feedbacks.js
@@ -4,7 +4,7 @@ const WHITE = combineRgb(255, 255, 255)
 const CARMINE_RED = combineRgb(153, 0, 51)
 
 export function getFeedbacks(self, choices) {
-	function muteFdb(label, { fg, bg }, choices, msb, offset) {
+	function muteFeedback(label, choices, msb, offset) {
 		return {
 			type: 'boolean',
 			name: `Mute ${label}`,
@@ -20,8 +20,8 @@ export function getFeedbacks(self, choices) {
 				},
 			],
 			defaultStyle: {
-				color: fg,
-				bgcolor: bg,
+				color: WHITE,
+				bgcolor: CARMINE_RED,
 			},
 			callback: (feedback, _context) => {
 				const key = `mute_${msb}.${parseInt(feedback.options.channel) + offset}`
@@ -31,14 +31,14 @@ export function getFeedbacks(self, choices) {
 	}
 
 	return {
-		mute_input: muteFdb('Input', { fg: WHITE, bg: CARMINE_RED }, choices.inputChannels, 0, 0),
-		mute_lr: muteFdb('LR', { fg: WHITE, bg: CARMINE_RED }, [{ label: `LR`, id: 0 }], 0, 68),
-		mute_aux: muteFdb('Aux', { fg: WHITE, bg: CARMINE_RED }, choices.mixesAndLR, 0, 69),
-		mute_group: muteFdb('Group', { fg: WHITE, bg: CARMINE_RED }, choices.groups, 0, 48),
-		mute_matrix: muteFdb('Matrix', { fg: WHITE, bg: CARMINE_RED }, choices.matrixes, 0, 85),
-		mute_dca: muteFdb('DCA', { fg: WHITE, bg: CARMINE_RED }, choices.dcas, 2, 0),
-		mute_fx_return: muteFdb('FX Return', { fg: WHITE, bg: CARMINE_RED }, choices.fxReturns, 0, 60),
-		mute_fx_send: muteFdb('FX Send', { fg: WHITE, bg: CARMINE_RED }, choices.fxSends, 0, 81),
-		mute_mutegroup: muteFdb('MuteGroup', { fg: WHITE, bg: CARMINE_RED }, choices.muteGroups, 4, 0),
+		mute_input: muteFeedback('Input', choices.inputChannels, 0, 0),
+		mute_lr: muteFeedback('LR', [{ label: `LR`, id: 0 }], 0, 68),
+		mute_aux: muteFeedback('Aux', choices.mixesAndLR, 0, 69),
+		mute_group: muteFeedback('Group', choices.groups, 0, 48),
+		mute_matrix: muteFeedback('Matrix', choices.matrixes, 0, 85),
+		mute_dca: muteFeedback('DCA', choices.dcas, 2, 0),
+		mute_fx_return: muteFeedback('FX Return', choices.fxReturns, 0, 60),
+		mute_fx_send: muteFeedback('FX Send', choices.fxSends, 0, 81),
+		mute_mutegroup: muteFeedback('MuteGroup', choices.muteGroups, 4, 0),
 	}
 }
