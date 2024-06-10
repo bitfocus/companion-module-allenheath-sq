330636fa1fa0136f915f73f10c39aab7dd8fb759
Jeff Walden <jwalden@mit.edu>
2024-06-10 14:55:34 -0700

    chore: Add a declaration file covering `sqInstance` and the fields added to it not in the class declaration.

---
+++ src/instance.d.ts
@@ -0,0 +1,69 @@
+import type { CompanionVariableValue, InstanceBase, SomeCompanionConfigField, TCPHelper } from '@companion-module/base'
+import type { SQInstanceConfig } from './config.js'
+import type { Model } from './mixer/model.js'
+
+export type ParamHalf = readonly [number, number]
+
+type Level = number | string
+
+declare class sqInstance extends InstanceBase<SQInstanceConfig> {
+	// Not declared, simply added in configUpdated
+	config: SQInstanceConfig
+
+	model: Model
+
+	readonly fdbState: { [key: string]: boolean }
+	readonly lastValue: { [key: string]: CompanionVariableValue }
+	mch: number
+
+	constructor(internal: unknown): InstanceBase<SQInstanceConfig>
+
+	dBToDec(lv: string | number, typ?: string)
+	decTodB(vc: number, vf: number, typ?: string)
+
+	async destroy(): Promise<void>
+	async init(config: SQInstanceConfig): Promise<void>
+
+	getConfigFields(): SomeCompanionConfigField[]
+
+	async configUpdated(config: SQInstanceConfig): Promise<void>
+
+	// Absent after construction, added by initTCP in api.js
+	midiSocket: TCPHelper | undefined
+
+	// Defined in api.js, added via Object.assign.
+	setRouting(ch: number, mix: readonly number[], ac: boolean, mc: number, oMB: ParamHalf, oLB: ParamHalf): Buffer[]
+	async setLevel(
+		ch: number,
+		mx: number,
+		ct: number,
+		lv: Level,
+		oMB: ParamHalf,
+		oLB: ParamHalf,
+		cnfg?: string,
+	): Promise<Buffer[]>
+	async setScene(val: number): Promise<number>
+	getLevel(
+		ch: number,
+		mx: number,
+		ct: number,
+		oMB: ParamHalf,
+		oLB: ParamHalf,
+	): { buffer: [Buffer]; channel: [number, number] }
+	async fadeLevel(
+		fd: number,
+		ch: number,
+		mx: number,
+		ct: number,
+		lv: Level,
+		oMB: ParamHalf,
+		oLB: ParamHalf,
+		cnfg?: string,
+	): Promise<Buffer[]>
+	sendSocket(buff: Buffer): void
+	getRemoteLevel(): void
+	getRemoteStatus(act: string): void
+	async getRemoteValue(data: number[]): Promise<void>
+	initTCP(): void
+	async sendBuffers(buffers: Buffer[]): Promise<void>
+}
