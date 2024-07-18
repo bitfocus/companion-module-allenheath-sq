52f2cc11f94a82eae67afbec0f0d1b1114125f99
Jeff Walden <jwalden@mit.edu>
2024-07-17 19:51:04 -0700

    fix: Add a neglected argument to a `getFadeTimeSeconds` call.

--- src/actions/old-output.js
+++ src/actions/old-output.js
@@ -40,7 +40,7 @@ export function oldOutputActions(self, mixer, choices, levelOption, fadingOption
 				fadingOption,
 			],
 			callback: async ({ options: opt }) => {
-				const fadeTimeSeconds = getFadeTimeSeconds(opt)
+				const fadeTimeSeconds = getFadeTimeSeconds(self, opt)
 				if (fadeTimeSeconds === null) {
 					return
 				}
