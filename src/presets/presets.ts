4575505248748bec76463da8ade48c56082c5730
Jeff Walden <jwalden@mit.edu>
2026-07-21 16:41:35 -0700

    refactor: Move talkback preset definitions to a separate file.

--- src/presets/presets.ts
+++ src/presets/presets.ts
@@ -1,104 +1,20 @@
-import type { Equal, Expect } from 'type-testing'
 import type { CompanionPresetDefinitions } from '@companion-module/base'
-import { AssignActionId } from '../actions/assign.js'
-import { LevelActionId } from '../actions/level.js'
 import { MuteActionId } from '../actions/mute.js'
-import { getTalkbackChannel } from '../config.js'
 import { MuteFeedbackId } from '../feedbacks/mute.js'
 import type { sqInstance } from '../instance.js'
-import { LR } from '../mixer/lr.js'
 import type { Model } from '../mixer/model.js'
 import { type NRPN, splitNRPN } from '../mixer/nrpn/nrpn.js'
 import { LevelNRPNCalculator } from '../mixer/nrpn/source-to-sink.js'
 import { mutePresets } from './mutes.js'
 import { Black, White } from '../utils/colors.js'
-import type { ZeroIndexed } from '../utils/indexed.js'
+import { talkbackPresets } from './talkback.js'
+import { getTalkbackChannel } from '../config.js'
 
 export function getPresets(instance: sqInstance, model: Model): CompanionPresetDefinitions {
 	const presets: CompanionPresetDefinitions = {
 		...mutePresets(model),
+		...talkbackPresets(getTalkbackChannel(instance.config), model),
 	}
-
-	/* TALKBACK*/
-	model.forEach('mix', (mix, mixLabel, mixDesc) => {
-		const talkbackChannelZeroIndexed = getTalkbackChannel(instance.config)
-		type assert_TalkbackChannelIsZeroIndexed = Expect<Equal<typeof talkbackChannelZeroIndexed, ZeroIndexed>>
-
-		presets[`preset_talkback_mix${mix}`] = {
-			type: 'button',
-			category: 'Talkback',
-			name: `Talk to ${mixDesc}`,
-			style: {
-				text: `Talk to ${mixLabel}`,
-				size: 'auto',
-				color: White,
-				bgcolor: Black,
-			},
-			steps: [
-				{
-					down: [
-						{
-							actionId: AssignActionId.InputChannelToMix,
-							options: {
-								inputChannel: talkbackChannelZeroIndexed,
-								mixAssign: [LR],
-								mixActive: false,
-							},
-						},
-						{
-							actionId: AssignActionId.InputChannelToMix,
-							options: {
-								inputChannel: talkbackChannelZeroIndexed,
-								mixAssign: [mix],
-								mixActive: true,
-							},
-						},
-						{
-							actionId: LevelActionId.InputChannelLevelInMixOrLR,
-							options: {
-								input: talkbackChannelZeroIndexed,
-								assign: mix,
-								level: 49,
-							},
-						},
-						{
-							actionId: MuteActionId.MuteInputChannel,
-							options: {
-								strip: talkbackChannelZeroIndexed,
-								mute: 2,
-							},
-						},
-					],
-					up: [
-						{
-							actionId: AssignActionId.InputChannelToMix,
-							options: {
-								inputChannel: talkbackChannelZeroIndexed,
-								mixAssign: [mix],
-								mixActive: false,
-							},
-						},
-						{
-							actionId: LevelActionId.InputChannelLevelInMixOrLR,
-							options: {
-								input: talkbackChannelZeroIndexed,
-								assign: mix,
-								level: 0,
-							},
-						},
-						{
-							actionId: MuteActionId.MuteInputChannel,
-							options: {
-								strip: talkbackChannelZeroIndexed,
-								mute: 1,
-							},
-						},
-					],
-				},
-			],
-			feedbacks: [],
-		}
-	})
 
 	/* MUTE + FADER LEVEL */
 	const muteWithFaderLevel = (
---
+++ src/presets/talkback.ts
@@ -0,0 +1,91 @@
+import type { CompanionPresetDefinitions } from '@companion-module/base'
+import { AssignActionId } from '../actions/assign.js'
+import { LevelActionId } from '../actions/level.js'
+import { MuteActionId } from '../actions/mute.js'
+import { LR } from '../mixer/lr.js'
+import type { Model } from '../mixer/model.js'
+import { White, Black } from '../utils/colors.js'
+import type { ZeroIndexed } from '../utils/indexed.js'
+
+export function talkbackPresets(talkbackChannel: ZeroIndexed, model: Model): CompanionPresetDefinitions {
+	const presets: CompanionPresetDefinitions = {}
+
+	model.forEach('mix', (mix, mixLabel, mixDesc) => {
+		presets[`preset_talkback_mix${mix}`] = {
+			type: 'button',
+			category: 'Talkback',
+			name: `Talk to ${mixDesc}`,
+			style: {
+				text: `Talk to ${mixLabel}`,
+				size: 'auto',
+				color: White,
+				bgcolor: Black,
+			},
+			steps: [
+				{
+					down: [
+						{
+							actionId: AssignActionId.InputChannelToMix,
+							options: {
+								inputChannel: talkbackChannel,
+								mixAssign: [LR],
+								mixActive: false,
+							},
+						},
+						{
+							actionId: AssignActionId.InputChannelToMix,
+							options: {
+								inputChannel: talkbackChannel,
+								mixAssign: [mix],
+								mixActive: true,
+							},
+						},
+						{
+							actionId: LevelActionId.InputChannelLevelInMixOrLR,
+							options: {
+								input: talkbackChannel,
+								assign: mix,
+								level: 49,
+							},
+						},
+						{
+							actionId: MuteActionId.MuteInputChannel,
+							options: {
+								strip: talkbackChannel,
+								mute: 2,
+							},
+						},
+					],
+					up: [
+						{
+							actionId: AssignActionId.InputChannelToMix,
+							options: {
+								inputChannel: talkbackChannel,
+								mixAssign: [mix],
+								mixActive: false,
+							},
+						},
+						{
+							actionId: LevelActionId.InputChannelLevelInMixOrLR,
+							options: {
+								input: talkbackChannel,
+								assign: mix,
+								level: 0,
+							},
+						},
+						{
+							actionId: MuteActionId.MuteInputChannel,
+							options: {
+								strip: talkbackChannel,
+								mute: 1,
+							},
+						},
+					],
+				},
+			],
+			feedbacks: [],
+		}
+	})
+
+	return presets
+}
