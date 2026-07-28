357e0b8d423f2db57d7685dbcd1991793464b6ac
Jeff Walden <jwalden@mit.edu>
2026-07-28 13:49:31 -0700

    refactor: Alter mute presets definition to be more amenable to 2.0 preset typing.

--- src/presets/mutes.ts
+++ src/presets/mutes.ts
@@ -1,72 +1,127 @@
-import type { CompanionPresetDefinitions } from '@companion-module/base'
-import { MuteActionId, StatusOptionId, StripOptionId } from '../actions/schemas/mute.js'
-import { MuteFeedbackId } from '../feedbacks/schemas/mute.js'
+import type {
+	CompanionButtonPresetDefinition,
+	CompanionButtonStyleProps,
+	CompanionFeedbackButtonStyleResult,
+	CompanionPresetAction,
+	CompanionPresetDefinitions,
+} from '@companion-module/base'
+import { type MuteActionId, StatusOptionId, StripOptionId } from '../actions/schemas/mute.js'
+import type { MuteFeedbackId } from '../feedbacks/schemas/mute.js'
 import type { Model } from '../mixer/model.js'
 import { MuteOperation } from '../types.js'
 import { White, Black } from '../utils/colors.js'
 
-// Doesn't this lint make *no sense* for intersections?  The intersection of two
-// types that *do not* duplicate is just `never`, which makes any such
-// intersection's result totally vacuous...right?
-// eslint-disable-next-line @typescript-eslint/no-duplicate-type-constituents
-type MuteType = keyof typeof MuteFeedbackId & keyof typeof MuteActionId
+const MutePresetStyleCommon = {
+	size: 'auto',
+	color: White,
+	bgcolor: Black,
+} as const satisfies Omit<CompanionButtonStyleProps, 'text'>
+
+const StatusOptionValue = MuteOperation.Toggle
+
+const MuteFeedbackStyle = {} as const satisfies CompanionFeedbackButtonStyleResult
+
+const EmptyStepList = [] as const satisfies CompanionPresetAction[]
+
+type NumberedSignalMuteType = Exclude<MuteFeedbackId & MuteActionId, 'mute_lr'>
+
+function createNumberedSignalMutePreset(
+	category: string,
+	label: string,
+	actionId: NumberedSignalMuteType,
+	n: number,
+): CompanionButtonPresetDefinition {
+	const name = `${label} ${n + 1}`
+	return {
+		type: 'button',
+		category,
+		name,
+		style: {
+			text: name,
+			...MutePresetStyleCommon,
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId,
+						options: {
+							[StripOptionId]: n + 1,
+							[StatusOptionId]: StatusOptionValue,
+						},
+					},
+				],
+				up: EmptyStepList,
+			},
+		],
+		feedbacks: [
+			{
+				feedbackId: actionId,
+				options: {
+					n: n + 1,
+				},
+				style: MuteFeedbackStyle,
+			},
+		],
+	}
+}
+
+// The LR preset action doesn't have a strip identification option, so define
+// this preset specially to omit it.
+const MuteLRPresetDefinition = {
+	type: 'button',
+	category: 'Mute Mix - Group',
+	name: `Mute LR`,
+	style: {
+		text: 'Mute LR',
+		...MutePresetStyleCommon,
+	},
+	steps: [
+		{
+			down: [
+				{
+					actionId: 'mute_lr',
+					options: {
+						[StatusOptionId]: MuteOperation.Toggle,
+					},
+				},
+			],
+			up: EmptyStepList,
+		},
+	],
+	feedbacks: [
+		{
+			feedbackId: 'mute_lr',
+			options: {},
+			style: MuteFeedbackStyle,
+		},
+	],
+} as const satisfies CompanionButtonPresetDefinition
 
 export function mutePresets(model: Model): CompanionPresetDefinitions {
 	const presets: CompanionPresetDefinitions = {}
 
 	/* MUTE */
-	const createtMute = (category: string, label: string, type: MuteType, count: number): void => {
+	const addMutePresetsForType = (
+		category: string,
+		label: string,
+		type: NumberedSignalMuteType,
+		count: number,
+	): void => {
 		for (let i = 0; i < count; i++) {
-			const suffix = count > 1 ? `_${i}` : ''
-			const maybeSpaceNum = count > 1 ? ` ${i + 1}` : ''
-			presets[`preset_${MuteActionId[type]}${suffix}`] = {
-				type: 'button',
-				category,
-				name: `${label}${maybeSpaceNum}`,
-				style: {
-					text: `${label}${maybeSpaceNum}`,
-					size: 'auto',
-					color: White,
-					bgcolor: Black,
-				},
-				steps: [
-					{
-						down: [
-							{
-								actionId: MuteActionId[type],
-								options: {
-									[StripOptionId]: i + 1,
-									[StatusOptionId]: MuteOperation.Toggle,
-								},
-							},
-						],
-						up: [],
-					},
-				],
-				feedbacks: [
-					{
-						feedbackId: MuteFeedbackId[type],
-						options:
-							MuteFeedbackId[type] === MuteFeedbackId.MuteLR
-								? {}
-								: {
-										n: i + 1,
-									},
-					},
-				],
-			}
+			presets[`preset_${type}_${i}`] = createNumberedSignalMutePreset(category, label, type, i)
 		}
 	}
 
-	createtMute('Mute Input', 'Input channel', 'MuteInputChannel', model.inputOutputCounts.inputChannel)
-	createtMute('Mute Mix - Group', 'LR', 'MuteLR', model.inputOutputCounts.lr)
-	createtMute('Mute Mix - Group', 'Mix', 'MuteMix', model.inputOutputCounts.mix)
-	createtMute('Mute Mix - Group', 'Group', 'MuteGroup', model.inputOutputCounts.group)
-	createtMute('Mute Mix - Group', 'Matrix', 'MuteMatrix', model.inputOutputCounts.matrix)
-	createtMute('Mute FX', 'FX Send', 'MuteFXSend', model.inputOutputCounts.fxSend)
-	createtMute('Mute FX', 'FX Return', 'MuteFXReturn', model.inputOutputCounts.fxReturn)
-	createtMute('Mute DCA', 'DCA', 'MuteDCA', model.inputOutputCounts.dca)
-	createtMute('Mute MuteGroup', 'MuteGroup', 'MuteMuteGroup', model.inputOutputCounts.muteGroup)
+	addMutePresetsForType('Mute Input', 'Input channel', 'mute_input', model.inputOutputCounts.inputChannel)
+	presets['preset_lr'] = MuteLRPresetDefinition
+	addMutePresetsForType('Mute Mix - Group', 'Mix', 'mute_aux', model.inputOutputCounts.mix)
+	addMutePresetsForType('Mute Mix - Group', 'Group', 'mute_group', model.inputOutputCounts.group)
+	addMutePresetsForType('Mute Mix - Group', 'Matrix', 'mute_matrix', model.inputOutputCounts.matrix)
+	addMutePresetsForType('Mute FX', 'FX Send', 'mute_fx_send', model.inputOutputCounts.fxSend)
+	addMutePresetsForType('Mute FX', 'FX Return', 'mute_fx_return', model.inputOutputCounts.fxReturn)
+	addMutePresetsForType('Mute DCA', 'DCA', 'mute_dca', model.inputOutputCounts.dca)
+	addMutePresetsForType('Mute MuteGroup', 'MuteGroup', 'mute_mutegroup', model.inputOutputCounts.muteGroup)
 
 	return presets
 }
